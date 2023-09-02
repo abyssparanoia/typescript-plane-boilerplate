948d19598d0e9b1978fa0d43f0c37e20df9e7676
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-09-03 01:23:10 +0900

    feat: impl factory di

--- packages/express-backend/src/domain/repository/user.ts
+++ packages/express-backend/src/domain/repository/user.ts
@@ -1,5 +1,8 @@
+import { Logger } from '../../util/logger'
 import { User } from '../model/user'
 import { BaseGetOptions } from './base'
+
+export type UserRepositoryFactory = (logger: Logger) => UserRepository
 
 export interface UserRepository {
   get(query: UserRepositoryGetQuery): Promise<User>
