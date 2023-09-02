93e62322c168a88448219de460910d525ce0a33c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-09-03 04:13:52 +0900

    feat: add openapi generator

--- packages/express-backend/src/infrastructure/http/run.ts
+++ packages/express-backend/src/infrastructure/http/run.ts
@@ -6,6 +6,7 @@ import { attachLogger } from './server/middleware/attach-logger'
 import { Logger } from '../../util/logger'
 import { requestLogging } from './server/middleware/request-logging'
 import { errorLogging } from './server/middleware/error-logging'
+import { initialize } from 'express-openapi'
 
 declare global {
   namespace Express {
@@ -20,18 +21,25 @@ export const runHttpServer = () => {
   const userHandler = new UserHandler(dependency.userInteractorFactory)
 
   const app = express()
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
   app.use(attachLogger)
+  app.use(requestLogging)
 
   app.get('/', (_req, res) => {
     res.send('hello world')
   })
-  app.get(
-    '/v1/users/:userId',
-    asyncWrapper((req, res) => userHandler.get(req, res))
-  )
 
-  app.use(requestLogging)
-  app.use(errorLogging)
+  initialize({
+    app: app,
+    apiDoc: './openapi/api.yaml',
+    validateApiDoc: true,
+    operations: {
+      getUser: asyncWrapper((req, res) => userHandler.get(req, res))
+    },
+    errorMiddleware: errorLogging,
+    promiseMode: true
+  })
   app.listen(environment.PORT, () => console.log(`Example app listening on port ${environment.PORT}!`))
 }
 
