4387874fcb1b2902451c12d0c787c60b5d12cc6f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-09-03 02:53:03 +0900

    feat: impl drizzle base

--- packages/express-backend/src/infrastructure/http/server/middleware/request-logging.ts
+++ packages/express-backend/src/infrastructure/http/server/middleware/request-logging.ts
@@ -1,29 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
-import { ApplicationError } from '../../../../util/application-error'
 
-export const requestLogging = async (req: Request, res: Response, next: NextFunction) => {
+export const requestLogging = (req: Request, res: Response, next: NextFunction) => {
   const { headers, body, url, method } = req
-  try {
-    await next()
+  next()
+  if (res.statusCode < 400) {
     req.logger.info(`${req.method}: ${req.url} -> ${res.statusCode}`, { headers, body, url, method })
-  } catch (err: any) {
-    if (err instanceof ApplicationError) {
-      const logArgs = {
-        headers,
-        body,
-        url,
-        method,
-        err: err.serializeToJson()
-      }
-      if (err.statusCode < 500) {
-        req.logger.warn(`${req.method}: ${req.url} -> ${err.statusCode}`, logArgs)
-      } else {
-        req.logger.error(`${req.method}: ${req.url} -> ${err.statusCode}`, logArgs)
-      }
-      res.status(err.statusCode).json(err.serializeToJson())
-    } else {
-      req.logger.error(`${req.method}: ${req.url} -> 500`, { headers, body, url, method, err: err.message })
-      res.status(500).json({ statusCode: 500, message: 'Internal Server Error' })
-    }
   }
 }
