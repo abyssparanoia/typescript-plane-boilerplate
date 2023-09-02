50c77545bea95cf11cae8f6ca56ce4d1014d0a83
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-09-03 02:53:07 +0900

    feat: impl drizzle base

--- packages/express-backend/src/infrastructure/mysql/repository/user.ts
+++ packages/express-backend/src/infrastructure/mysql/repository/user.ts
@@ -1,26 +1,34 @@
 import { UserRepository, UserRepositoryFactory, UserRepositoryGetQuery } from '../../../domain/repository/user'
 import { User } from '../../../domain/model/user'
 import { Logger } from '../../../util/logger'
+import { MySqlClient } from '../client'
+import { ApplicationError } from '../../../util/application-error'
 
-export const newUserRepositoryFactory = (): UserRepositoryFactory => {
+export const newUserRepositoryFactory = (cli: MySqlClient): UserRepositoryFactory => {
   return (logger: Logger): UserRepository => {
-    return newUserRepository(logger)
+    return newUserRepository(logger, cli)
   }
 }
 
-const newUserRepository = (logger: Logger): UserRepository => new UserRepositoryImpl(logger)
+const newUserRepository = (logger: Logger, cli: MySqlClient): UserRepository => new UserRepositoryImpl(logger, cli)
 
 class UserRepositoryImpl implements UserRepository {
-  constructor(readonly logger: Logger) {}
+  constructor(readonly logger: Logger, private readonly cli: MySqlClient) {}
 
-  public async get(_: UserRepositoryGetQuery): Promise<User> {
-    const user = {
-      id: '1',
-      name: 'John',
-      email: '',
-      createdAt: new Date(),
-      updatedAt: new Date()
+  public async get(query: UserRepositoryGetQuery): Promise<User | undefined> {
+    const dbUser = await this.cli.query.users.findFirst({ where: (users, { eq }) => eq(users.id, query.id!) })
+    if (!dbUser) {
+      if (query.orFail) {
+        throw new ApplicationError(`user ${query.id!} not found`, 404)
+      }
+      return undefined
     }
-    return user
+    return {
+      id: dbUser.id,
+      name: dbUser.name,
+      email: dbUser.email,
+      createdAt: dbUser.createdAt,
+      updatedAt: dbUser.updatedAt
+    }
   }
 }
