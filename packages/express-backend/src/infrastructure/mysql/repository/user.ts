e04e10b01623959d606b0ac9cf135b6bd7fbef00
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-09-03 01:23:18 +0900

    feat: impl factory di

--- packages/express-backend/src/infrastructure/mysql/repository/user.ts
+++ packages/express-backend/src/infrastructure/mysql/repository/user.ts
@@ -1,10 +1,17 @@
-import { UserRepository, UserRepositoryGetQuery } from '../../../domain/repository/user'
+import { UserRepository, UserRepositoryFactory, UserRepositoryGetQuery } from '../../../domain/repository/user'
 import { User } from '../../../domain/model/user'
+import { Logger } from '../../../util/logger'
 
-export const newUserRepository = (): UserRepository => new UserRepositoryImpl()
+export const newUserRepositoryFactory = (): UserRepositoryFactory => {
+  return (logger: Logger): UserRepository => {
+    return newUserRepository(logger)
+  }
+}
+
+const newUserRepository = (logger: Logger): UserRepository => new UserRepositoryImpl(logger)
 
 class UserRepositoryImpl implements UserRepository {
-  constructor() {}
+  constructor(readonly logger: Logger) {}
 
   public async get(_: UserRepositoryGetQuery): Promise<User> {
     const user = {
