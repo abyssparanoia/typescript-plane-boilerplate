0c1b7b948758449c62920d0624f8e0cc4a857014
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-09-05 01:29:22 +0900

    feat: impl transaction callback

--- packages/express-backend/src/domain/model/user.ts
+++ packages/express-backend/src/domain/model/user.ts
@@ -1,17 +1,19 @@
 import { generateId } from '../../util/id'
 
-export type User = {
+export class User {
   id: string
   name: string
   email: string
   createdAt: Date
   updatedAt: Date
+
+  constructor(id: string, name: string, email: string, createdAt: Date, updatedAt: Date) {
+    this.id = id
+    this.name = name
+    this.email = email
+    this.createdAt = createdAt
+    this.updatedAt = updatedAt
+  }
 }
 
-export const newUser = (name: string, email: string, t: Date): User => ({
-  id: generateId(),
-  name,
-  email,
-  createdAt: t,
-  updatedAt: t
-})
+export const newUser = (name: string, email: string, t: Date): User => new User(generateId(), name, email, t, t)
