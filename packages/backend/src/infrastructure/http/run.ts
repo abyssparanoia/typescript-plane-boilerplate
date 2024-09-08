e53d28f6742b38130dbda9b050b28d8f1d2c0079
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-09-08 21:19:18 +0900

    chore: update some packages

---
+++ packages/backend/src/infrastructure/http/run.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+import { environment } from '../enviroment'
+import { newDependency } from '../dependency/dependency'
+import { UserHandler } from './server/handler/user'
+import { attachLogger } from './server/middleware/attach-logger'
+import { Logger } from '../../util/logger'
+import { requestLogging } from './server/middleware/request-logging'
+import { errorLogging } from './server/middleware/error-logging'
+import { initialize } from 'express-openapi'
+
+declare global {
+  namespace Express {
+    interface Request {
+      logger: Logger
+    }
+    interface Response {}
+  }
+}
+export const runHttpServer = () => {
+  const dependency = newDependency()
+  const userHandler = new UserHandler(dependency.userInteractorFactory)
+
+  const app = express()
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(attachLogger)
+  app.use(requestLogging)
+
+  app.get('/', (_req, res) => {
+    res.send('hello world')
+  })
+
+  initialize({
+    app: app,
+    apiDoc: './openapi/api.yaml',
+    validateApiDoc: true,
+    operations: {
+      getUser: (req, res) => userHandler.get(req, res)
+    },
+    errorMiddleware: errorLogging,
+    promiseMode: true
+  })
+  app.listen(environment.PORT, () => console.log(`Example app listening on port ${environment.PORT}!`))
+}
