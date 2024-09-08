5e81552d4ee548b734fb4d6c027fabaf1d9820b7
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-09-09 00:03:48 +0900

    refactor: formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -12,7 +15,9 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -25,10 +30,18 @@ export default [
     }
   },
   {
+    plugins: {
+      prettier: pluginPrettier
+    },
     rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/no-unused-vars': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/unified-signatures': 'off'
     }
   },
   {
