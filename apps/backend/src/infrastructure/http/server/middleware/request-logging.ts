2986d8c93f19c0efe11862a35cc923eee2082841
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-09-10 01:33:12 +0900

    refactor: move backend to apps

---
+++ apps/backend/src/infrastructure/http/server/middleware/request-logging.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const requestLogging = (req: Request, res: Response, next: NextFunction) => {
+  const { headers, body, url, method } = req
+  next()
+  res.on('finish', () => {
+    if (res.statusCode < 400) {
+      req.logger.info(`${req.method}: ${req.url} -> ${res.statusCode.toString()}`, { headers, body, url, method })
+    }
+  })
+}
